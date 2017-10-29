39a7a1e664a00b21c092eed833601acdc924ccfc
orlovw <orlownikita265@gmail.com>
2017-10-29 21:47:31 +0200

    main & setup

--- main.py
+++ main.py
@@ -49,5 +49,5 @@ def test():
     return 'working'
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 88))
+    port = int(os.environ.get('PORT', 443))
     app.run(host='0.0.0.0', port=port)
